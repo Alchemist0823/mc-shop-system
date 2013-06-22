09790230696cb16d2b8b560f1eae6b366f11e6de
Liyiming Ke <dustocyno@gmail.com>
2013-06-22 23:21:54 +0800

    Add PlayerListener

--- src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/MainPlugin.java
+++ src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/MainPlugin.java
@@ -8,10 +8,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.n8lm.MCShopSystemPlugin.config.*;
-import com.n8lm.MCShopSystemPlugin.server.CommunicationServer;
+import com.n8lm.MCShopSystemPlugin.server.CommunicationServer;import com.n8lm.MCShopSystemPlugin.Listener.PlayerListener;
+
 
 /**
  * @author Alchemist
@@ -23,6 +25,8 @@ public final class MainPlugin extends JavaPlugin {
 	private static MainPlugin plugin;
 	private static Server bukkitServer;
 	private static CommunicationServer server;
+	
+	private final PlayerListener playerListener = new PlayerListener(this);
 	
 	private static Settings settings;
 	
@@ -65,12 +69,22 @@ public final class MainPlugin extends JavaPlugin {
 		if (settings.isServerActive())
 		{
 			server = new CommunicationServer();
-			server.start();// TODO Insert code to start CommunicationServer
+			try{
+				server.startServer();
+			}
+			catch (IOException ex){
+				logger.info("Mcshop failed to start Server.");
+				logger.log(Level.SEVERE, null, ex);
+				return;
+			}
 		}
 		
-		
+		// Register Listener
+		bukkitServer.getPluginManager().registerEvents(playerListener, plugin);
+
 		this.getLogger().info("MC Shop System is enable");
-        // TODO Insert logic to be performed when the plugin is enabled
+		// TODO Insert logic to be performed when the plugin is enabled
+		
     }
  
     @Override
@@ -81,7 +95,9 @@ public final class MainPlugin extends JavaPlugin {
 			server.stopServer();
 		}
 		this.getLogger().info("MC Shop System is disable");
+		
         // TODO Insert logic to be performed when the plugin is disabled
+		PlayerInteractEvent.getHandlerList().unregister(plugin);
     }
     
     public static MainPlugin getInstance()
