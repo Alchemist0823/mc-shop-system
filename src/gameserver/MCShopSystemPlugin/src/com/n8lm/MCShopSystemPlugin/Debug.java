c092e28832228bae9d8acd357122f3f1974dabc5
Liyiming Ke <dustocyno@gmail.com>
2013-06-22 22:23:46 +0800

    Update Debug

---
+++ src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/Debug.java
@@ -0,0 +1,11 @@
+package com.n8lm.MCShopSystemPlugin;
+
+import java.util.logging.Level;
+
+public class Debug{
+	public static void log(Level level, String msg){
+		if(MainPlugin.getSettings().isDebugMode()){
+			MainPlugin.getMainLogger().log(level, msg);
+		}
+	}
+}
