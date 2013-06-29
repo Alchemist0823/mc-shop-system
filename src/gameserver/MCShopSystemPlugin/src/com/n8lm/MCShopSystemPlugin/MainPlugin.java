62b3fbf1fabbae239e1efc79f0a84c1ca30f88c7
Kelym <dustocyno@gmail.com>
2013-06-29 15:44:27 +0800

    Update

--- src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/MainPlugin.java
+++ src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/MainPlugin.java
@@ -14,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.n8lm.MCShopSystemPlugin.config.*;
 import com.n8lm.MCShopSystemPlugin.server.CommunicationServer;
 import com.n8lm.MCShopSystemPlugin.FileOperator.PasswordOperator;
+import com.n8lm.MCShopSystemPlugin.FileOperator.WaitListOperator;
 import com.n8lm.MCShopSystemPlugin.Listener.PlayerListener;
 
 public final class MainPlugin extends JavaPlugin {
