c424ff7c6461d0b9d633601530b3153a734e1751
WSX <709083882@qq.com>
2013-07-06 22:36:29 +0800

    upload

--- src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/packets/GetPlayerDataHandlerPacket.java
+++ src/gameserver/MCShopSystemPlugin/src/com/n8lm/MCShopSystemPlugin/packets/GetPlayerDataHandlerPacket.java
@@ -12,10 +12,7 @@ import com.n8lm.MCShopSystemPlugin.MainPlugin;
 import com.n8lm.MCShopSystemPlugin.server.CommunicationHelper;
 import com.n8lm.MCShopSystemPlugin.server.PacketHandler;
 import com.n8lm.MCShopSystemPlugin.FileOperator.CheckUser;
-/**
- * @author Alchemist
- *
- */
+
 public class GetPlayerDataHandlerPacket extends PacketHandler {
 
 	/**
